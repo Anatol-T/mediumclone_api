fc32876bbc9fb492b6ad39038f36aaa9c1fa044e
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2023-03-11 22:18:01 +0800

    eslint prettier

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { TagController } from './tag.controller';
+import { Module } from '@nestjs/common'
+import { TagController } from './tag.controller'
 
 @Module({
   controllers: [TagController],
