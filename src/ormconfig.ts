a7b15c176f7b590fe9b90051a200ba6b2acfbbf5
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2023-03-16 11:10:32 +0800

    creating user entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,6 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 import { TagEntity } from './tag/tag.entity'
+import { UserEntity } from './user/user.entity'
 
 export const config: PostgresConnectionOptions = {
   type: 'postgres',
@@ -8,7 +9,9 @@ export const config: PostgresConnectionOptions = {
   username: 'admin',
   password: '123',
   database: 'mediumclone',
-  entities: [TagEntity],
+  entities: [TagEntity, UserEntity],
+  // entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  // migrations: ['/migrations/**/*{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
 }
