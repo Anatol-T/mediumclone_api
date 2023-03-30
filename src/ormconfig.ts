aa68c752eb0b045b760a55c9639be0f4a8856e8d
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2023-03-30 21:01:21 +0800

    article module

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,4 +1,7 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
+import { ArticleEntity } from './article/article.entity'
+import { UserEntity } from './user/user.entity'
+import { TagEntity } from './tag/tag.entity'
 
 console.log([__dirname.split('\\').join('/') + '/migrations/**/*{.ts,.js}'])
 export const config: PostgresConnectionOptions = {
@@ -8,7 +11,8 @@ export const config: PostgresConnectionOptions = {
   username: 'admin',
   password: '123',
   database: 'mediumclone',
-  entities: [__dirname + '*/**/*.entity{.ts,.js}'],
+  //entities: [__dirname + '*/**/*.entity{.ts,.js}'],
+  entities: [ArticleEntity, UserEntity, TagEntity],
   synchronize: true,
   // migrations: ['/migrations/**/*{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
